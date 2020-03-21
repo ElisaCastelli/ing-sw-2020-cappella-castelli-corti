0ad3c4737d06263c456cbe587bcf2b2c052229f6
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-03-21 15:04:02 +0100

    Elisa

---
+++ src/test/java/it/polimi/ingsw/AppTest2.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest2 {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
