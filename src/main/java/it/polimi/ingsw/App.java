1c1071240d489f3353b90b93d058a8853f168861
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2020-03-21 15:22:37 +0100

    ilaria Prova

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -9,7 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(" Greta");
+        System.out.println("Greta");
         System.out.println( "Elisa" );
+        System.out.println( "Ilaria" );
     }
 }
