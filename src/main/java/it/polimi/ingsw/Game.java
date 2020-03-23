768745d66935a452128fd22b07d9fde80270f79d
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-03-23 09:53:40 +0100

    Prove varie

--- src/main/java/it/polimi/ingsw/Game.java
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -1,29 +1,33 @@
 package it.polimi.ingsw;
 
+import java.util.Scanner;
+
 public class Game {
     private Board board;
     private Gamer gamers[];
     private  int nPlayer;
 
-
     Game(){
         board= new Board();
         gamers= new Gamer[3];
-        for(int i=0;i<3;i++){
-            gamers[i]=new Gamer();
-        }
         nPlayer=0;
     }
-    /*public void build(int r, int c){
-        board.build(r,c);
+    Game(int nPlayer){
+        board= new Board();
+        gamers= new Gamer[3];
+        this.nPlayer=nPlayer;
     }
-    public void print(){
-        board.print();
-    }*/
+
     public void play(){
         //ricevo partecipanti
-
+           for(int i=0;i<nPlayer;i++){
+               System.out.println("Nome giocatore "+i+" :");
+               Scanner input = new Scanner(System.in);
+               String s = input.nextLine();
+               gamers[i]=new Gamer(s);
+           }
         //getsisco i turni
+
     }
     /*public static void main( String[] args )
     {
--- src/main/java/it/polimi/ingsw/Gamer.java
+++ src/main/java/it/polimi/ingsw/Gamer.java
@@ -1,4 +1,27 @@
 package it.polimi.ingsw;
 
 public class Gamer {
+    private static String name;
+    private boolean alive;
+    //Worker myWorkers[];
+
+    Gamer(String name){
+        this.name=name;
+        //myWorkers= new Worker[2];
+        //myworkers[1]=new Worker(1);
+        //myWorkers[2]=new Worker(2);
+        alive=true;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+/*void move(Worker w){
+
+    }*/
+
 }
