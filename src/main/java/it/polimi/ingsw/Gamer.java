691dd39aff50bcf79557b65a23cf77cbcfa9c10f
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-03-23 10:15:17 +0100

    Prima bozza Gamer

--- src/main/java/it/polimi/ingsw/Gamer.java
+++ src/main/java/it/polimi/ingsw/Gamer.java
@@ -1,8 +1,11 @@
 package it.polimi.ingsw;
 
+import java.util.Scanner;
+
 public class Gamer {
     private static String name;
     private boolean alive;
+    private God myCard;
     //Worker myWorkers[];
 
     Gamer(String name){
@@ -11,17 +14,47 @@ public class Gamer {
         //myworkers[1]=new Worker(1);
         //myWorkers[2]=new Worker(2);
         alive=true;
+        myCard=null;
     }
-
+    public God getMyCard() {
+        return myCard;
+    }
+    public void setMyCard(God myCard) {
+        this.myCard = myCard;
+    }
     public boolean isAlive() {
         return alive;
     }
-
     public void setAlive(boolean alive) {
         this.alive = alive;
     }
-/*void move(Worker w){
+    void move(){
+        boolean workerMoved=false;
+        boolean built=false;
+        Box b=new Box();
+        while(!workerMoved){
+            b.clear();
+            //la richiesta del numero di pedina e dello spazio dove voglio muovermi verrà fatta graficamente
+            System.out.println("Pedina da muovere [1] 0 [2]:");
+            Scanner worker = new Scanner(System.in);
+            int w = Integer.parseInt(worker.nextLine());
+            System.out.println("Riga dove voglio muovermi:");
+            Scanner row = new Scanner(System.in);
+            int r = Integer.parseInt(row.nextLine());
+            System.out.println("Colonna dove voglio muovermi:");
+            Scanner col = new Scanner(System.in);
+            int c = Integer.parseInt(row.nextLine());
+            b.setRow(r);
+            b.setColumn(c);
+            /*if(worker.getPos().reachable(b)){
+                myCard.moveWorker(workers[w],b);
+                workerMoved=true;
+            }*/
+        }
+        while(!built){
 
-    }*/
+        }
+    }
+
 
 }
