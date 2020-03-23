48b80c5c9fb3427b0b11cc214c687eee4be8843a
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-03-23 16:38:06 +0100

    cambio reachable

--- src/main/java/it/polimi/ingsw/Game.java
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -1,32 +1,31 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     private Board board;
-    private Gamer gamers[];
+    private ArrayList<Gamer> gamers;
     private  int nPlayer;
 
     Game(){
         board= new Board();
-        gamers= new Gamer[3];
+        gamers= new ArrayList<>();
         nPlayer=0;
-    }
-    Game(int nPlayer){
-        board= new Board();
-        gamers= new Gamer[3];
-        this.nPlayer=nPlayer;
     }
 
     public void play(){
+        Gamer g;
         //ricevo partecipanti
-           for(int i=0;i<nPlayer;i++){
-               System.out.println("Nome giocatore "+i+" :");
-               Scanner input = new Scanner(System.in);
-               String s = input.nextLine();
-               gamers[i]=new Gamer(s);
-           }
-        //getsisco i turni
+        for(int i=0;i<nPlayer;i++){
+            System.out.println("Nome giocatore "+i+" :");
+            Scanner input = new Scanner(System.in);
+            String s = input.nextLine();
+            g=new Gamer("s");
+            gamers.add(i,g);
+        }
+        //gestisco i turni
+
 
     }
     /*public static void main( String[] args )
