6d05601f79f7bbdd0b6e53492e1e27155b2eb1b6
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2020-03-24 17:59:10 +0100

    ho fatto i test di Worker e Board

--- src/test/java/it/polimi/ingsw/BoardTest.java
+++ src/test/java/it/polimi/ingsw/BoardTest.java
@@ -11,15 +11,14 @@ public class BoardTest extends TestCase {
     }
 
     public void testClear() {
-        Worker workerTest=new Worker(1,boardTest);
+        Worker workerTest=new Worker(1,boardTest.getBox(4,3));
         boardTest.getBox(1,1).setWorker(workerTest);
         boardTest.build(2,3);
         boardTest.build(2,3);
         boardTest.clear();
         assertTrue(boardTest.getBox(1,1).notWorker());
-        assertEquals(0,boardTest.getBox(2,3).getCounter());
+        assertEquals(0,boardTest.getBox(4,3).getCounter());
         boardTest.build(0,0);
-        workerTest.setBoard(boardTest);
         workerTest.setWorkerId(2);
         boardTest.getBox(0,0).setWorker(workerTest);
         boardTest.clear();
--- src/test/java/it/polimi/ingsw/WorkerTest.java
+++ src/test/java/it/polimi/ingsw/WorkerTest.java
@@ -4,8 +4,8 @@ import junit.framework.TestCase;
 
 public class WorkerTest extends TestCase {
     Board boardTest=new Board();
-    Worker workerTest1= new Worker(1,boardTest);
-    Worker workerTest2=new Worker(2,boardTest);
+    Worker workerTest1= new Worker(1,boardTest.getBox(1,1));
+    Worker workerTest2=new Worker(2,boardTest.getBox(2,2));
 
 
     public void testGetHeight() {
@@ -13,25 +13,27 @@ public class WorkerTest extends TestCase {
         assertEquals(0,workerTest1.getHeight());
         workerTest1.setHeight(1);
         assertEquals(1,workerTest1.getHeight());
-        workerTest1.setHeight(2);
-        assertEquals(2,workerTest1.getHeight());
-        workerTest1.setHeight(3);
-        assertEquals(3,workerTest1.getHeight());
+        workerTest2.setHeight(2);
+        assertEquals(2,workerTest2.getHeight());
+        workerTest2.setHeight(3);
+        assertEquals(3,workerTest2.getHeight());
     }
 
     public void testGetWorkerId() {
         workerTest1.setWorkerId(1);
         assertEquals(1,workerTest1.getWorkerId());
+        workerTest1.setWorkerId(2);
+        assertEquals(2,workerTest2.getWorkerId());
     }
 
     public void testGetActualBox() {
-        workerTest1.setWorkerId(2);
-        assertEquals(2,workerTest1.getWorkerId());
+        assertEquals(boardTest.getBox(1,1),workerTest1.getActualBox());
+        assertEquals(boardTest.getBox(2,2),workerTest2.getActualBox());
     }
 
-    public void testInitializePos() {
+    /*public void testInitializePos() {
         assertTrue(workerTest1.initializePos(1, 1));
         assertTrue(workerTest2.initializePos(1,2));
         assertFalse(workerTest1.initializePos(1,2));
-    }
+    }*/
 }
