355b164a4dc2d5d1317798c2946ef740ae49b024
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-03-24 14:49:50 +0100

    main

--- src/main/java/it/polimi/ingsw/Worker.java
+++ src/main/java/it/polimi/ingsw/Worker.java
@@ -6,51 +6,42 @@ public class Worker {
     private Box actualBox;
     private Board board;
 
-
     public Worker(){
         workerId=0;
         height=0;
         actualBox=null;
         board=null;
     }
-
     public Worker(int workerId, Board board){
         this.workerId=workerId;
         this.board=board;
     }
-
     public Worker(int workerId, Box actualBox) {
         this.workerId = workerId;
         height=0;
         this.actualBox=actualBox;
     }
-
     public int getHeight() {
         return height;
     }
     public void setHeight(int height) {
         this.height = height;
     }
-
     public int getWorkerId() {
         return workerId;
     }
-
     public void setWorkerId(int workerId) {
         this.workerId = workerId;
     }
     public void setBoard(Board board) {
         this.board = board;
     }
-
     public Box getActualBox() {
         return actualBox;
     }
-
     public void setActualBox(Box actualBox) {
         this.actualBox = actualBox;
     }
-
     public boolean initializePos(int row, int column){
         Box requestedBox;
         if(board.isEmpty(row,column)){
@@ -64,6 +55,5 @@ public class Worker {
             return false;
         }
     }
-
 
 }
