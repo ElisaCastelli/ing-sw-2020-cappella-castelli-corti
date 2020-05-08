8ffe35296918a31227debada49ec6ab68f4c8504
elisacastelli <elisa3.castelli@mail.polimi.it>
2020-05-08 12:03:29 +0200

    Aggiunto notify nel controller

--- src/main/java/it/polimi/ingsw/server/Controller.java
+++ src/main/java/it/polimi/ingsw/server/Controller.java
@@ -20,29 +20,43 @@ public class Controller  {
     }
     public void chooseCard(int playerIndex, int godCard) throws Exception {
         gameModel.chooseCard(playerIndex, godCard);
+        gameModel.notifyAddCard(playerIndex);
     }
     public boolean initializeWorker(int indexPlayer, int indexWorker, Box box) {
-        return gameModel.initializeWorker(indexPlayer, indexWorker, box);
+        boolean init= gameModel.initializeWorker(indexPlayer, indexWorker, box);
+        gameModel.notifyAddWorker();
+        return init;
     }
 
     public void startTurn(int indexPlayer) {
         gameModel.startTurn(indexPlayer);
+        gameModel.notifyPlayerStateStart(indexPlayer);
     }
     public void setBoxReachable(int indexPlayer, int indexWorker) {
         gameModel.setBoxReachable(indexPlayer, indexWorker);
+        gameModel.notifySetReachable();
     }
     public boolean movePlayer(int indexPlayer, int indexWorker, int row, int column) {
-        return gameModel.movePlayer(indexPlayer, indexWorker, row, column);
+        boolean moved=gameModel.movePlayer(indexPlayer, indexWorker, row, column);
+        gameModel.notifyMovedWorker();
+        return moved;
     }
     public void setBoxBuilding(int indexPlayer, int indexWorker) {
         gameModel.setBoxBuilding(indexPlayer, indexWorker);
+        gameModel.notifySetBuilding();
     }
     public boolean buildBlock(int indexPlayer, int indexWorker, int row, int column) {
-        return gameModel.buildBlock(indexPlayer, indexWorker, row, column);
+        boolean built=gameModel.buildBlock(indexPlayer, indexWorker, row, column);
+        gameModel.notifyBuildBlock();
+        return built;
     }
     public void finishTurn(int indexPlayer) {
         gameModel.finishTurn(indexPlayer);
+        gameModel.notifyPlayerStateWaiting(indexPlayer);
     }
+
+
+
     public boolean checkWin(int indexPlayer, Box startBox, int indexWorker) {
         return gameModel.checkWin(indexPlayer, startBox, indexWorker);
     }
@@ -51,6 +65,7 @@ public class Controller  {
     }
     public void setWinningPlayer(int indexPlayer) {
         gameModel.setWinningPlayer(indexPlayer);
+
     }
     public void setDeadPlayer(int indexPlayer){
         gameModel.setDeadPlayer(indexPlayer);
